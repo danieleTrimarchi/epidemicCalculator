d81b98b5ed8720f2403ec7aced64c85315d793e0
Daniele Trimarchi <daniele.trimarchi@gmail.com>
2020-04-15 21:58:28 +0200

    switch to Matrix representation

--- SEIR2.py
+++ SEIR2.py
@@ -18,7 +18,9 @@ import matplotlib.pyplot as plt
 #   Rm: Individuals recoveded from mild symptoms
 #   Rh: Individuals recoveded from hospitalisation
 #   Rd: Death Individuals
-idx = ['S','E','I','Im','Ih','Id','R','Rm','Rh','Rd']
+idx = ['S','E','I','Im','Is','Id','R','Rm','Rs','Rd']
+def i(dofStr):
+    return idx.index(dofStr)
 
 # -- INITIAL CONDITIONS -- :
 N = 7.e6    # Population
@@ -26,75 +28,78 @@ Tinc = 5.2  # Incubation period [days]
 Tinf = 2.9  # Time the patient is infectious [days]
 Rt = 2.2    # Reproduction number (how many people infected by each person)
 pd = 0.02   # Case death rate. Percentage of the infected patients who will die
-ph = 0.2    # Hospitalization rate. Percentage of the infected patients who will be hospitalized
+ps = 0.2    # Hospitalization rate. Percentage of the infected patients who will be hospitalized
+pm = 1. - ps - pd   # Mild rate. Percentage of the infected patients with mild symptoms
 Tmild = 11.1 # Recovery time for mild cases
-THosp = 28.6 # Length of hospital stay [days]
+TSevere = 28.6 # Length of hospital stay [days]
+THospLag = 5 # Lag between Infection of severe case and hospitalisation [days]
 TDeath= 32   # Time from end of incubation to death [days]
 
-beta = Rt / Tinf   #
-gamma = 1. / Tinf  # infectious rate [1/days]
-sigma = 1. / Tinc  # incubation rate [1/days]
-gamma_m = 1. / Tmild
-gamma_h = 1. / THosp
-gamma_d = 1. / TDeath
-pm = 1. - ph - pd   # Mild rate. Percentage of the infected patients with mild symptoms
-
-
 # -- EQUATIONS --
-#   dS(t)/dt = - beta S(t) I(t)
-#   dE(t)/dt = beta S(t) I(t) - sigma E(t)
+#   dS(t)/dt = -(Rt/Tinf) * S(t) I(t)
+#   dE(t)/dt =  (Rt/Tinf) * S(t) I(t) - 1/Tinc E(t)
 # --
-#   dI(t)/dt = sigma E(t) - gamma I(t)
-#  this is the sum of three separated contributions:
-#   dIm(t)/dt = pm * gamma I(t) - gamma_m * Im(t)
-
-# \!/ ----------------------------------------------------- \!/
-# \!/ ADD Severe_H \!/
-#   dIh(t)/dt = ph * gamma I(t) - gamma_h * Ih(t)
-# this is severe. to be added: the severe hospitalized.
-# There is a lag between the severe and its hospitalisation
-# \!/ ----------------------------------------------------- \!/
-
-#   dId(t)/dt = pd * gamma I(t) - gamma_d * Id(t)
+#   dI(t)/dt = 1/Tinc * E(t) - 1/Tinf I(t)
+#   this is the sum of three separated contributions:
+#       dIm(t)/dt = pm * 1/Tinf * I(t) - 1/Tmild     * Im(t)
+#       dIs(t)/dt = ps * 1/Tinf * I(t) - 1/T_hospLag * Is(t)
+#       dId(t)/dt = pd * 1/Tinf * I(t) - 1/Tdead     * Id(t)
 # --
-#   dR(t)/dt = gamma I(t)
+#  Hp: mild cases recover
+#     some of the severe cases will be hospitalized after some time lag:
+#       dIsh(t)/dt = 1/T_hospLag * Is(t) - 1/Tsevere * Ish(t)
+#     deadly cases just recover when dying
+# --
+#  dR(t)/dt = 1/Tinf I(t)
 #  this is the sum of three separated contributions:
-#   dRm(t)/dt = gamma_m Im(t)
-#   dRh(t)/dt = gamma_h Ih(t)
-#   dRd(t)/dt = gamma_d Id(t)
+#   dRm(t)/dt = 1/Tmild   * Im(t)
+#   dRs(t)/dt = 1/Tsevere * Ish(t)
+#   dRd(t)/dt = 1/Tdead   * Id(t)
 
+# def x = [       S              E          I        Im         Is         Id        R        Rm   Rs   Rd ]^T
+# and A =
+#  S      [-Rt/(Tinf*N)*I(t)                                                                               ]
+#  E      [ Rt/(Tinf*N)*I(t)  -1/Tinc                                                                      ]
+#  I      [                    1/Tinc    -1/Tinf                                                           ]
+#  Im     [                             pm*1/Tinf  -1/Tmild                                                ]
+#  Is     [                             ps*1/Tinf            -1/Thosp                                      ]
+#  Id     [                             pd*1/Tinf                       -1/Tdead                           ]
+#  R      [                               1/Tinf                                      0                    ]
+#  Rm     [                                         1/Tmild                                   0            ]
+#  Rs     [                                                   1/Thosp                               0      ]
+#  Rd     [                                                              1/Tdead                         0 ]
+A = np.zeros((len(idx), len(idx)))
+# --
+A[i('E'),i('E')] = -1. / Tinc
+A[i('I'),i('E')] =  1. / Tinc
+# --
+A[i('I'),i('I')]  = -1. / Tinf
+A[i('Im'),i('I')] = pm * 1. / Tinf
+A[i('Is'),i('I')] = ps * 1. / Tinf
+A[i('Id'),i('I')] = pd * 1. / Tinf
+A[i('R'),i('I')]  =  1. / Tinf
+# --
+A[i('Im'),i('Im')] = -1. / Tmild
+A[i('Rm'),i('Im')] =  1. / Tmild
+# --
+A[i('Is'),i('Is')] = -1. / THospLag
+A[i('Rs'),i('Is')] =  1. / THospLag
+# --
+A[i('Id'),i('Id')] = -1. / TDeath
+A[i('Rd'),i('Id')] =  1. / TDeath
 
-# Remember: z=[S,E,I,Im,Ih,Id,R,Rm,Rh,Rd]
 def model(t,z):
 
-    S = z[idx.index('S')]
-    E = z[idx.index('E')]
-    I = z[idx.index('I')]
-    Im = z[idx.index('Im')]
-    Ih = z[idx.index('Ih')]
-    Id = z[idx.index('Id')]
-    R = z[idx.index('R')]
-    Rm = z[idx.index('Rm')]
-    Rh = z[idx.index('Rh')]
-    Rd = z[idx.index('Rd')]
+    # print("z[i('I')]=",z[i('I')])
 
-    #print("R0= ",(a*beta)/((mu+a)*mu+gamma))
+    # Update non const part of the matrix A
+    A[i('S'),i('S')]= - Rt/(Tinf*N) * z[i('I')]
+    A[i('E'),i('S')]=   Rt/(Tinf*N) * z[i('I')]
 
-    dSdt = - beta * I * S / N
-    dEdt = beta * I * S / N - sigma * E
-
-    dIdt = sigma * E - gamma * I
-    dImdt = pm * gamma * I - gamma_m * Im
-    dIhdt = ph * gamma * I - gamma_h * Ih
-    dIddt = pd * gamma * I - gamma_d * Id
-
-    dRdt = gamma * I
-    dRmdt = gamma_m * Im
-    dRhdt = gamma_h * Ih
-    dRddt = gamma_d * Id
-
-    dzdt= [dSdt,dEdt,dIdt,dImdt,dIhdt,dIddt,dRdt,dRmdt,dRhdt,dRddt]
-    return dzdt
+    # print("A=", A)
+    # print("z=",z)
+    # print("A * z=",np.dot(A,z))
+    return np.dot(A,z)
 
 
 S0 = N      # Initial population
@@ -102,21 +107,20 @@ E0 = 0.     # Exposed individuals @t0
 I0 = 1.     # Infected individuals @t0
 R0 = 0.
 
-#    [S0, E0, I0,   Im0,  Ih0,  Id0, R0,  Rm0,  Rh0,  Rd0]
-y0 = [S0, E0, I0, I0*pm,I0*ph,I0*pd, R0,R0*pm,I0*ph,I0*pd]
+#     S  E  I  Im    Is    Id    R  Rm    Rs    Rd
+x0 = [S0,E0,I0,I0*pm,I0*ps,I0*pd,R0,R0*pm,R0*ps,R0*pd]
 
 # solve ODE in the timespan t=[0,200]
-z = solve_ivp(model,[0,200],y0)
+z = solve_ivp(model,[0,200],x0)
 
 # plot results
 plt.subplot(211)
 
-plt.plot(z.t,z.y[idx.index('S')],'b-',label=r'S')
-plt.plot(z.t,z.y[idx.index('E')],'y-',label=r'E')
-plt.plot(z.t,z.y[idx.index('I')],'r-',label=r'I')
-plt.plot(z.t,z.y[idx.index('R')],'g-',label=r'R')
+plt.plot(z.t,z.y[i('S')],'b-',label=r'S')
+plt.plot(z.t,z.y[i('E')],'y-',label=r'E')
+plt.plot(z.t,z.y[i('I')],'r-',label=r'I')
+plt.plot(z.t,z.y[i('R')],'g-',label=r'R')
 plt.ylabel('response')
-
 
 #plt.yscale("log")
 
@@ -128,14 +132,14 @@ plt.plot(ref[0],ref[2],'yx',label=r'$E_{ref}$')
 plt.plot(ref[0],ref[3],'rx',label=r'$I_{ref}$')
 plt.plot(ref[0],ref[4],'gx',label=r'$R_{ref}$')
 
-plt.title(r'R_0=$\frac{\beta} {\gamma}=$'+ str(beta/(gamma)))
+plt.title(r'R_0=$'+ str(Rt))
 plt.legend(loc='center left',
            fontsize='xx-small')
 
 plt.subplot(212)
 
 # Compute the fatalities
-plt.plot(z.t,z.y[idx.index('Rd')],'k-',label=r'Rd')
+plt.plot(z.t,z.y[i('Rd')],'k-',label=r'Rd')
 plt.plot(ref[0],ref[5],'kx',label=r'$fatalities_{ref}$')
 plt.legend(loc='center left',
            fontsize='xx-small')
